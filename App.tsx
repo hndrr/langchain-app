9a7c66c33e7adef5b2e8d874e01158b422caa3e1
hndrr <yozhik20@gmail.com>
2023-10-09 00:26:04 +0900

    fix

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 // import { OPENAI_API_KEY } from "module:react-native-dotenv";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  GestureResponderEvent,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { useState, useCallback } from "react";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { HumanChatMessage } from "langchain/schema";
+import { HumanMessage } from "langchain/schema";
 
 import "text-encoding-polyfill";
 import "react-native-url-polyfill/auto";
+import "web-streams-polyfill/ponyfill";
 
 export default function App() {
   const [text, setText] = useState<string>("");
@@ -14,19 +22,19 @@ export default function App() {
 
   const OPENAI_API_KEY = process.env.EXPO_PUBLIC_OPENAI_API_KEY;
 
-  const handleSubmit = useCallback(async () => {
+  const handleSubmit = async (event: GestureResponderEvent) => {
     setOutput("");
-    const model = new ChatOpenAI({
-      openAIApiKey: OPENAI_API_KEY,
-      modelName: "gpt-3.5-turbo",
-    });
-    try {
-      const response = await model.call([new HumanChatMessage(text)]);
-      setOutput(response.text);
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
+    // const model = new ChatOpenAI({
+    //   openAIApiKey: OPENAI_API_KEY,
+    //   modelName: "gpt-3.5-turbo",
+    // });
+    // try {
+    //   const response = await model.call([new HumanMessage(text)]);
+    //   setOutput(response.content);
+    // } catch (e) {
+    //   console.error(e);
+    // }
+  };
 
   return (
     <View style={styles.container}>
@@ -34,10 +42,10 @@ export default function App() {
         returnKeyType="send"
         style={styles.text}
         value={text}
-        placeholder="promptを入力してください。"
+        placeholder="promptを入力"
         onChangeText={setText}
-        onSubmitEditing={handleSubmit}
       />
+      <Button title="送信" onPress={handleSubmit} />
       <Text style={styles.text}>{output}</Text>
       <StatusBar style="auto" />
     </View>
