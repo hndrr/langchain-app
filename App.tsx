2dbce4923fae8622737116c6247cbc2db09b324d
hndrr <yozhik20@gmail.com>
2023-10-15 02:36:29 +0900

    fix: ScrollView追加

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 import { config } from "@gluestack-ui/config";
 
 import { StatusBar } from "expo-status-bar";
-import { Button, ButtonText, Text, Box } from "@gluestack-ui/themed";
+import { Button, ButtonText, Text, ScrollView } from "@gluestack-ui/themed";
 import { GestureResponderEvent, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { postChatMessage } from "./postChatMessage";
@@ -41,88 +41,92 @@ export default function App() {
   return (
     <GluestackUIProvider config={config}>
       <VStack width="$full" height="$full" paddingHorizontal="$5">
-        <Heading size="lg" marginTop={60}>
+        <Heading size="lg" marginTop={60} paddingBottom={10}>
           Simple Chatbot
         </Heading>
-        {outputs.length > 0 &&
-          outputs.map((output, index) => {
-            if (!output) {
-              return <></>;
-            }
+        <ScrollView h="$80" w="$full" marginBottom={"$40"}>
+          {outputs.length > 0 &&
+            outputs.map((output, index) => {
+              if (!output) {
+                return <></>;
+              }
 
-            const isAI = output._getType() === "ai";
-            const isUser = output._getType() === "human";
+              const isAI = output._getType() === "ai";
+              const isUser = output._getType() === "human";
 
-            return (
-              <HStack
-                space="md"
-                key={index}
-                width="$full"
-                padding="$2"
-                borderRadius="$md"
-                bgColor="$gray100"
-                justifyContent={isAI ? "flex-start" : "flex-end"}
-              >
-                {isAI && (
-                  <Avatar
-                    size="md"
-                    bgColor="$indigo300"
-                    borderColor="$indigo600"
-                    borderWidth={2}
-                  >
-                    <Icon as={User} color="$indigo600" size="xl" />
-                  </Avatar>
-                )}
-                <Text
-                  width="auto"
-                  maxWidth={isUser ? "80%" : "$80"}
-                  padding="$1"
-                  textAlign={isUser ? "right" : "left"}
+              return (
+                <HStack
+                  space="md"
+                  key={index}
+                  width="$full"
+                  padding="$2"
                   borderRadius="$md"
-                  borderWidth={1}
+                  bgColor="$gray100"
+                  justifyContent={isAI ? "flex-start" : "flex-end"}
                 >
-                  {output.content}
-                </Text>
-                {isUser && (
-                  <Avatar
-                    size="md"
-                    bgColor="$amber300"
-                    borderColor="$amber600"
-                    borderWidth={2}
+                  {isAI && (
+                    <Avatar
+                      size="md"
+                      bgColor="$indigo300"
+                      borderColor="$indigo600"
+                      borderWidth={2}
+                    >
+                      <Icon as={User} color="$indigo600" size="xl" />
+                    </Avatar>
+                  )}
+                  <Text
+                    width="auto"
+                    maxWidth={isUser ? "80%" : "$80"}
+                    padding="$1"
+                    textAlign={isUser ? "right" : "left"}
+                    borderRadius="$md"
+                    borderWidth={1}
                   >
-                    <Icon as={User} color="$amber600" size="xl" />
-                  </Avatar>
-                )}
-              </HStack>
-            );
-          })}
-        <HStack
-          space="md"
-          position="absolute"
-          bottom={"$20"}
-          width="$full"
-          left="$3"
-        >
-          <Input
-            variant="outline"
-            size="lg"
-            isDisabled={false}
-            isInvalid={false}
-            isReadOnly={false}
-            width="$4/5"
-          >
-            <InputField
-              placeholder="promptを入力"
-              returnKeyType="send"
-              value={text}
-              onChangeText={setText}
-            />
-          </Input>
-          <Button size="lg" onPress={handleSubmit} width="$20" bg="$red500">
-            <ButtonText>送信</ButtonText>
-          </Button>
-        </HStack>
+                    {output.content}
+                  </Text>
+                  {isUser && (
+                    <Avatar
+                      size="md"
+                      bgColor="$amber300"
+                      borderColor="$amber600"
+                      borderWidth={2}
+                    >
+                      <Icon as={User} color="$amber600" size="xl" />
+                    </Avatar>
+                  )}
+                </HStack>
+              );
+            })}
+        </ScrollView>
       </VStack>
+      <HStack
+        space="md"
+        position="absolute"
+        bottom="$4"
+        paddingBottom={"$20"}
+        width="$full"
+        marginHorizontal={10}
+        bg="$white"
+      >
+        <Input
+          variant="outline"
+          size="lg"
+          isDisabled={false}
+          isInvalid={false}
+          isReadOnly={false}
+          width="$4/6"
+        >
+          <InputField
+            placeholder="promptを入力"
+            returnKeyType="send"
+            value={text}
+            onChangeText={setText}
+          />
+        </Input>
+        <Button size="lg" onPress={handleSubmit} width="$1/4" bg="$red500">
+          <ButtonText>送信</ButtonText>
+        </Button>
+      </HStack>
       <StatusBar style="auto" />
     </GluestackUIProvider>
   );
