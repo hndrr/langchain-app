45fc4588964c8e90046f3ec44da7cf877838011d
hndrr <yozhik20@gmail.com>
2023-10-14 22:48:55 +0900

    fix: chatbot修正

--- App.tsx
+++ App.tsx
@@ -19,43 +19,64 @@ import { GestureResponderEvent, StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { postChatMessage } from "./postChatMessage";
 import { User } from "lucide-react-native";
+import { BaseMessage } from "langchain/schema";
 
 export default function App() {
   const [text, setText] = useState<string>("");
-  const [output, setOutput] = useState<string>("");
+  const [outputs, setOutputs] = useState<BaseMessage[]>([]);
 
   const handleSubmit = async (event: GestureResponderEvent) => {
-    setOutput("");
     const response = await postChatMessage(text);
-    setOutput(response?.content ?? "");
+    setOutputs([...outputs, response as BaseMessage]);
     setText("");
   };
 
   return (
     <GluestackUIProvider config={config}>
-      <VStack width="$full" height="$full" space="md" paddingHorizontal="$10">
+      <VStack width="$full" height="$full" paddingHorizontal="$5">
         <Heading size="lg" marginTop={60}>
           Chatbot
         </Heading>
-        <HStack space="md" height="$1/2">
-          <Avatar
-            size="md"
-            bgColor="$indigo300"
-            borderColor="$indigo600"
-            borderWidth={2}
-          >
-            <Icon as={User} color="$indigo600" size="xl" />
-          </Avatar>
-          <Text width="$80" padding="$1">
-            {output}
-          </Text>
-        </HStack>
+        {outputs.length > 0 &&
+          outputs.map((output, index) => {
+            if (!output) {
+              return <></>;
+            }
+
+            const isAI = output._getType() === "ai";
+            const isUser = output._getType() === "human";
+
+            return (
+              <HStack
+                space="md"
+                key={index}
+                width="$full"
+                padding="$2"
+                borderRadius="$md"
+                bgColor="$gray100"
+              >
+                {isAI && (
+                  <Avatar
+                    size="md"
+                    bgColor="$indigo300"
+                    borderColor="$indigo600"
+                    borderWidth={2}
+                  >
+                    <Icon as={User} color="$indigo600" size="xl" />
+                  </Avatar>
+                )}
+                <Text width="$80" padding="$1">
+                  {output.name} {output.content}
+                </Text>
+              </HStack>
+            );
+          })}
         <HStack
           space="md"
           position="absolute"
           bottom={"$20"}
           width="$full"
-          left="$10"
+          left="$3"
         >
           <Input
             variant="outline"
