d82599bd886c08565e6482a45b9a7a76489e659f
hndrr <yozhik20@gmail.com>
2023-10-14 22:09:05 +0900

    fix: GlueStackUI追加

--- App.tsx
+++ App.tsx
@@ -1,18 +1,24 @@
 import "text-encoding-polyfill";
 import "react-native-url-polyfill/auto";
 import "web-streams-polyfill/dist/polyfill.min.js";
+import {
+  Avatar,
+  GluestackUIProvider,
+  HStack,
+  Heading,
+  Icon,
+  Input,
+  InputField,
+  VStack,
+} from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
 
 import { StatusBar } from "expo-status-bar";
-import {
-  Button,
-  GestureResponderEvent,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
-import { useState } from "react";
+import { Button, ButtonText, Text, Box } from "@gluestack-ui/themed";
+import { GestureResponderEvent, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import { postChatMessage } from "./postChatMessage";
+import { User } from "lucide-react-native";
 
 export default function App() {
   const [text, setText] = useState<string>("");
@@ -22,21 +28,57 @@ export default function App() {
     setOutput("");
     const response = await postChatMessage(text);
     setOutput(response?.content ?? "");
+    setText("");
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        returnKeyType="send"
-        style={styles.text}
-        value={text}
-        placeholder="promptを入力"
-        onChangeText={setText}
-      />
-      <Button title="送信" onPress={handleSubmit} />
-      <Text style={styles.text}>{output}</Text>
+    <GluestackUIProvider config={config}>
+      <VStack width="$full" height="$full" space="md" paddingHorizontal="$10">
+        <Heading size="lg" marginTop={60}>
+          Chatbot
+        </Heading>
+        <HStack space="md" height="$1/2">
+          <Avatar
+            size="md"
+            bgColor="$indigo300"
+            borderColor="$indigo600"
+            borderWidth={2}
+          >
+            <Icon as={User} color="$indigo600" size="xl" />
+          </Avatar>
+          <Text width="$80" padding="$1">
+            {output}
+          </Text>
+        </HStack>
+        <HStack
+          space="md"
+          position="absolute"
+          bottom={"$20"}
+          width="$full"
+          left="$10"
+        >
+          <Input
+            variant="outline"
+            size="lg"
+            isDisabled={false}
+            isInvalid={false}
+            isReadOnly={false}
+            width="$4/5"
+          >
+            <InputField
+              placeholder="promptを入力"
+              returnKeyType="send"
+              value={text}
+              onChangeText={setText}
+            />
+          </Input>
+          <Button size="lg" onPress={handleSubmit} width="$20" bg="$red500">
+            <ButtonText>送信</ButtonText>
+          </Button>
+        </HStack>
+      </VStack>
       <StatusBar style="auto" />
-    </View>
+    </GluestackUIProvider>
   );
 }
 
